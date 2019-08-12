2dc2a15607021a61de85b4fad28f79e09b48aac6
dylan326 <dylanoldham326@gmail.com>
2019-08-12 10:26:23 -0400

    Work aug 12th 2019

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,38 @@
 @section('content')
 <div class="container">
 <div class = "row">
-  <div class="col-3 p-5">
-    <img src="https://image.freepik.com/free-photo/cityscape-icon-symbol-web-element_1150-1791.jpg" style="height: 350px; width: 350px;" class="rounded-circle" >
+  <div class="col-3 pt-5">
+    <img src="https://image.freepik.com/free-photo/cityscape-icon-symbol-web-element_1150-1791.jpg" style="height: 200px; width: 200px;" class="rounded-circle" >
   </div>
-  <div class="col-9">
+  <div class="col-9 pt-5">
 
     <div><h1>mockInstagram</h1></div>
+      <div class = "d-flex">
+        <div class="pr-5"><strong>153</strong> Posts</div>
+        <div class="pr-5"><strong>23k</strong> followers</div>
+        <div class="pr-5"><strong>212</strong> following</div>
+
+      </div>
+      <div class="pt-4 font-weight-bold">mockInstragram.org</div>
+      <div>Hello user, this is Dylan's mockInstagram website.  It's a cool website where you can exchage pictures and the like.  A fun place to
+      pictures with your friends and family</div>
+      <a href = "#">mockInstagram.org</a>
 
   </div>
 </div>
+<div class ="row pt-5">
+  <div class="col-4">
+    <img src="https://scontent-lga3-1.cdninstagram.com/vp/eac0707297d29b2dfa40c303d449836d/5DC94098/t51.2885-15/e35/61555594_471501460067844_6848849947845320953_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com" class="w-100">
+  </div>
+  <div class="col-4">
+    <img src="https://scontent-lga3-1.cdninstagram.com/vp/eac0707297d29b2dfa40c303d449836d/5DC94098/t51.2885-15/e35/61555594_471501460067844_6848849947845320953_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com" class="w-100">
+  </div>
+  <div class="col-4">
+    <img src="https://scontent-lga3-1.cdninstagram.com/vp/eac0707297d29b2dfa40c303d449836d/5DC94098/t51.2885-15/e35/61555594_471501460067844_6848849947845320953_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com" class="w-100">
+  </div>
+ 
+
+</div>
+
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mock Gram Aug 8th
+                    Mock Gram Aug 12VsCodeth
                 </div>
 
                 <div class="links">
