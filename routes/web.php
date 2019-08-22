de14d9aae24491ed5a7d0cc5b8c2f82951bd83ef
dylan326 <dylanoldham326@gmail.com>
2019-08-22 10:38:03 -0400

    Work Aug 22nd

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 //erased route
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
