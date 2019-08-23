a208942b095264f33a2dadd529f7b4b927293475
dylan326 <dylanoldham326@gmail.com>
2019-08-23 11:25:31 -0400

    Work aug 23rd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,16 +20,17 @@
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                   <!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
-                    </button>
+                    </button>-->
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                      <div><img src="svg/freeCodeCampLogo.svg" style="height: 20px;" class="pr-3;">&nbsp; | mockInstagram</div>
+                      <div><img src="/svg/freeCodeCampLogo.svg" style="height: 20px;" class="pr-3;">&nbsp; | mockInstagram</div>
+                     
 
                     </a>
                 </div>
@@ -38,6 +39,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
+                       
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -49,7 +51,7 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->username }} <!--<span class="caret"></span>-->
+                                    {{ Auth::user()->username }} <span></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
